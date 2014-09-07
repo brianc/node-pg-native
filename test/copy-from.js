c2ab9c1b76326afd36b852014c03d3b5b8e55cab
Brian M. Carlson <brian.m.carlson@gmail.com>
2014-09-07 15:31:49 -0400

    Readable stream working

--- test/copy-from.js
+++ test/copy-from.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var Client = require('../');
 
-return console.log('Streams make me crpy')
 describe('COPY FROM', function() {
   before(function(done) {
     this.client = Client()
@@ -12,22 +11,37 @@ describe('COPY FROM', function() {
     this.client.end(done);
   });
 
-  it.only('works', function(done) {
+  it('works', function(done) {
     var client = this.client;
     this.client.querySync('CREATE TEMP TABLE blah(name text, age int)');
     this.client.querySync('COPY blah FROM stdin');
-    console.log(this.client.pq.resultStatus())
     var stream = this.client.getCopyFromStream();
     stream.write(Buffer('Brian\t32\n', 'utf8'));
     stream.write(Buffer('Aaron\t30\n', 'utf8'));
-    stream.end(Buffer('Shelley\t28\n'), 'utf8');
+    stream.write(Buffer('Shelley\t28\n', 'utf8'));
+    stream.end();
 
     stream.once('finish', function() {
-      console.log('finish')
-      var rows = client.querySync('SELECT COUNT(*) FROM blah')
+      var rows = client.querySync('SELECT COUNT(*) FROM blah');
       assert.equal(rows.length, 1);
-      //assert.equal(rows[0].count, 3);
+      assert.equal(rows[0].count, 3);
       done();
     });
-  })
+  });
+
+  it('works with a callback passed to end', function(done) {
+    var client = this.client;
+    this.client.querySync('CREATE TEMP TABLE boom(name text, age int)');
+    this.client.querySync('COPY boom FROM stdin');
+    var stream = this.client.getCopyFromStream();
+    stream.write(Buffer('Brian\t32\n', 'utf8'));
+    stream.write(Buffer('Aaron\t30\n', 'utf8'), function() {
+      stream.end(Buffer('Shelley\t28\n', 'utf8'), function() {
+        var rows = client.querySync('SELECT COUNT(*) FROM boom')
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].count, 3);
+        done();
+      });
+    });
+  });
 });
