a60bd0f64f9136a818566574f32f20c904e8cd39
Brian M. Carlson <brian.m.carlson@gmail.com>
2015-01-20 10:45:41 -0500

    Update libpq

--- test/many-connections.js
+++ test/many-connections.js
@@ -1,19 +1,22 @@
 var Client = require('../');
 var async = require('async');
 var ok = require('okay');
+var bytes = require('crypto').pseudoRandomBytes
 
 describe('many connections', function() {
 
   describe('async', function() {
     var test = function(count, times) {
       it('connecting ' + count + ' clients ' + times, function(done) {
-        this.timeout(10000);
+        this.timeout(200000);
 
         var connectClient = function(n, cb) {
           var client = new Client();
           client.connect(ok(cb, function() {
-            client.query('SELECT NOW()', ok(cb, function() {
-              client.end(cb);
+            bytes(1000, ok(cb, function(chunk) {
+              client.query('SELECT $1::text as txt', [chunk.toString('base64')], ok(cb, function(rows) {
+                client.end(cb);
+              }));
             }));
           }));
         }
@@ -30,6 +33,7 @@ describe('many connections', function() {
     test(1, 1);
     test(1, 1);
     test(1, 1);
+    test(5, 5);
     test(5, 5);
     test(5, 5);
     test(5, 5);
